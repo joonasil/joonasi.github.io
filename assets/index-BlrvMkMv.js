dba70b8b2f7f465033a7f48ad217a074fdf98ad5
unknown <joonas.ilvonen@outlook.com>
2024-09-06 22:54:14 +0300

    test build

---
+++ assets/index-BlrvMkMv.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-B2KwCkIX.js";import{M as e}from"./Meta-DQxgR41M.js";import{F as t,T as a}from"./App-v4ybJfDx.js";import"./index-B63JMsT0.js";import"./index.esm-JHX1fJfl.js";function p(){return r.jsxs(r.Fragment,{children:[r.jsx(e,{title:"page 3"}),r.jsx(t,{children:r.jsx(a,{variant:"h3",children:"Page 3"})})]})}export{p as default};
